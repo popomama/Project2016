4ae63d97f7b9ef412910d465a44942602a280ffc
popomama <wei_xia@hotmail.com>
2016-02-19 00:06:49 -0500

    added 2nd solution for the path sum

--- Project2016/TreeGraph/CrackCode_TreeGraph.cs
+++ Project2016/TreeGraph/CrackCode_TreeGraph.cs
@@ -66,7 +66,7 @@ namespace Project2016.TreeGraph
 
         }
 
-        //Rando Node: You are implementing a binary search tree class from scratch, which , in addition
+        //Random Node: You are implementing a binary search tree class from scratch, which , in addition
         //to insert, find and delete, has a method getRandomNode() which returns a random node from the tree. 
         //all nodes should be eaually likely to be choen. Design and implement an algorighm for
         //getRandomNode, and explain how you would implement the rest of the methods.
@@ -145,5 +145,114 @@ namespace Project2016.TreeGraph
                     return this.Right.getIthNode(i - size-1);
             }
         }
+
+
+        // Path with Sum: Given a binary tree in whihch each node contains an integer value(which might be)
+        // positive or negative). Design an algorithm to count the number of paths that sum to a given value.
+        // the path doesn't need to start or end at the root or a leaf, but it must go downwards (travelling 
+        // only from parent nodes to child nodes.
+        int countPathWithSum(TreeNode root, int sum)
+        {
+            // the idea is to use recursion
+            // complexity: avg case: O(NLogN), depth is LogN,  CountPathwithSum will be called  by NLogN times
+            //            worst case: O(N^2) , tree with straight line down, to each node, the CountPathWith sum will be
+            // called 1+2+3+...+N times, so total is O(N^2)
+            int totalcount = 0;
+            totalcount = countPathWithSum(root, sum, 0);
+            
+            if(root!=null)
+            {
+                totalcount += countPathWithSum(root.Left, sum); // starting from the left
+                // totalcount += countPathWithSum(root.Left, sum, 0); can't use this one, as otherwise we will not be able to comeback
+                totalcount += countPathWithSum(root.Right, sum); // starting from the right
+
+            }
+
+            return totalcount;
+
+
+        }
+
+        int countPathWithSum(TreeNode root, int sum, int currentSum)
+        {
+            int total=0;
+            if (root == null)
+                return 0;
+
+            currentSum+=root.Value;
+            if (sum == currentSum )
+                total += 1;
+
+
+            total += countPathWithSum(root.Left, sum, currentSum);
+            total += countPathWithSum(root.Right, sum, currentSum);
+
+            return total;
+        }
+
+
+        //Path with Sum:  solution2
+        //use the hashcode to store the runningsum, check the hashtable to compare if there is available runningsum 
+        //equals the current runningsum - target running sum
+        //the time complexity is O(N) since each node will be visited only once, the average space cost is O(logN) and
+        //worst space cost is O(N).
+        int countPathWithSum2(TreeNode root, int targetSum)
+        {
+            if (root == null)
+                return 0;
+
+            //the key is the running sum while the value is the count of the running sum
+            Dictionary<int, int> AvailableRunningSum = new Dictionary<int, int>();
+
+            return countPathWithSum2(root, targetSum, 0, AvailableRunningSum);
+
+        }
+
+        int countPathWithSum2(TreeNode currentNode, int targetSum, int currentRunningSum, Dictionary<int,int> availableRunningSum)
+        {
+            if(currentNode==null)
+                return 0;
+
+            int totalCount = 0;
+
+            currentRunningSum+=currentNode.Value;
+
+            totalCount = availableRunningSum[currentRunningSum - targetSum];//check the hash table to see how many existingRunningsum with the 
+            //value of currentRunningSum - targetSum
+
+            if (targetSum == currentRunningSum)//we find a match
+            {
+                totalCount++;
+                IncrementAvailableRunningSum(availableRunningSum, targetSum, 1);
+            }
+            else
+            {
+              //  totalCount = availableRunningSum[currentRunningSum - targetSum];
+                //totalCount++;
+                IncrementAvailableRunningSum(availableRunningSum, currentRunningSum, 1);
+
+            }
+            totalCount += countPathWithSum2(currentNode.Left, targetSum, currentRunningSum, availableRunningSum);
+            totalCount += countPathWithSum2(currentNode.Right, targetSum, currentRunningSum, availableRunningSum);
+
+            IncrementAvailableRunningSum(availableRunningSum, currentRunningSum, -1);
+
+            return totalCount;
+        }
+
+        void IncrementAvailableRunningSum(Dictionary<int,int> availableRunningSum, int key, int delta)
+        {
+            if (availableRunningSum.ContainsKey(key))
+            {
+                availableRunningSum[key] += delta;
+                if (availableRunningSum[key] == 0)
+                    availableRunningSum.Remove(key);
+            }
+            else
+            {
+                availableRunningSum.Add(key, delta);
+            }
+        }
     }
+
 }
