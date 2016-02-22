32035a6142fa2789ded83709a884e9bd4553d333
popomama <wei_xia@hotmail.com>
2016-02-22 14:27:18 -0500

    added the signature of searchRoteatedSortedArray

---
+++ Project2016/SortingSeraching/CodeCrack_SortingSearching.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project2016.SortingSeraching
+{
+    class CodeCrack_SortingSearching
+    {
+
+        //Search In Rotaed Array: Given a sorted array of n integers tha has been rotaed an unknown number of times,
+        //write code to find an elemennt in the array. You may assume that array was originally sorted in increasing order.
+        // Input find 5 in {15,16,19,20,25,1,3,4,5,7,10,14}
+        //Output 8(index of 5)
+        int searchRotatedSortedArray(int[] arr, int value)
+        { }
+    }
+}
