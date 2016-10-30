bb9c8a8b9d6f89f4a76b3bddfa0a2c070f449e0c
Wei Xia <wei_xia@hotmail.com>
2016-10-29 17:12:59 -0700

    added

--- Project2016/Array/Misc.cs
+++ Project2016/Array/Misc.cs
@@ -54,5 +54,17 @@ namespace Project2016.Array
 
             Console.WriteLine();
         }
+
+
+        //Find a triplet that sum to a given value
+        //Given an array and a value, find if there is a triplet in array whose sum is equal to the given value.
+        //If there is such a triplet present in array, then print the triplet and return true. Else return false. 
+        //For example, if the given array is {12, 3, 4, 1, 6, 9} and given sum is 24, then there is a triplet(12, 3 and 9) 
+        //present in array whose sum is 24.
+
+
+        //Find 
+        //e. The input is a collection, C, of integers, and we are interested in a subset whose sum
+        //exactly half of the total sum of C. The problem is NP­hard
     }
 }
